70b6413c0db2902b39ae43989d01b3f83d883015
Temirlan <temirlanmukatayev@gmail.com>
2024-04-05 17:33:00 +0500

    Course CRUD ready

--- core/urls.py
+++ core/urls.py
@@ -17,13 +17,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
 
 if settings.DEBUG == True:
