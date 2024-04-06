b1d027f40984b2e278ab88e86c362b5a876f584d
Temirlan <temirlanmukatayev@gmail.com>
2024-04-06 11:52:21 +0500

    urls updated

--- courses/urls.py
+++ courses/urls.py
@@ -5,25 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.HomepageView.as_view(), name='home'),
-    path(
-        'courses/mine/',
-        views.ManageCourseListView.as_view(),
-        name='course_list'
-    ),
-    path(
-        'create/',
-        views.CourseCreateView.as_view(),
-        name='course_create'
-    ),
-    path(
-        '<pk>/edit/',
-        views.CourseUpdateView.as_view(),
-        name='course_edit'
-    ),
-    path(
-        '<pk>/delete/',
-        views.CourseDeleteView.as_view(),
-        name='course_delete'
-    ),
+    path('courses/mine/', views.ManageCourseListView.as_view(),
+         name='course_list'),
+    path('create/', views.CourseCreateView.as_view(), name='course_create'),
+    path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
+    path('<pk>/delete/', views.CourseDeleteView.as_view(),
+         name='course_delete'),
     path('lesson/list/', views.LessonListView.as_view(), name='lesson_list'),
 ]
