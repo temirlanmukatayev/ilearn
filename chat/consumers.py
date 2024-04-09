b40d8ff0ddd36c3a4d889c76228248a162dd9174
Temirlan <temirlanmukatayev@gmail.com>
2024-04-09 19:06:04 +0500

    Separate async chats added for every course

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,16 +1,39 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
+from datetime import datetime, timezone
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
 
-    def connect(self):
-        self.accept()
+    async def connect(self):
+        self.user = self.scope['user']
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = f'chat_{self.id}'
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        self.send(text_data=json.dumps({'message': message}))
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': self.user.username,
+                'datetime': datetime.now(timezone.utc).isoformat().replace("+00:00", "Z"),
+            }
+        )
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps(event))
--- core/settings.py
+++ core/settings.py
@@ -163,3 +163,13 @@ REST_FRAMEWORK = {
 
 # for channels
 ASGI_APPLICATION = 'core.asgi.application'
+
+# for redis
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)]
+        },
+    },
+}
